e2f044f3e23cff5a6b44e70b5898e9ec3909af03
Célia Richy <celiarichy@hotmail.com>
2018-09-24 18:53:55 +0200

    Exo 3 part 1 done!

---
+++ functions.R
@@ -0,0 +1,28 @@
+# Sum values in a column of a data frame.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d, provided as a string
+
+# RETURN VALUE:
+# if the specified column exists and contains numbers, returns the sum of
+# all values in the column; otherwise, returns NULL
+sum_column <- function(d, var) {
+  # Set a default value to return
+  result <- NULL
+  x <- d[[var]] # Remember, as we showed in class: there are two ways
+  # to access a column, and this is one; you should try
+  # and figure out why the other way won't work here,
+  # but that's not part of the homework
+  if (!is.null(x)) { # This tests to see whether the column exists in
+    # d; again, you should try and find a way to test
+    # this out for yourself to verify that it's true,
+    # but that's not part of the homework
+    result <- sum(d[[var]])
+  }
+    # You will need to do a bit of research in order to figure out how
+    # to test whether a vector contains numbers.
+  # YOUR CODE HERE: return the result
+  return(result)
+}
+
